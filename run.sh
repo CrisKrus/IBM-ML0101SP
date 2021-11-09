a366739a3c5defd1d1f87e9109257acf31a70f1d
Cristian Suarez <cristian.suarez.ulpgc@gmail.com>
2021-11-09 18:03:15 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 touch .env
 
-docker build -t cristian_suarez_dev/jupyterlab -f .docker/jupyter/Dockerfile .
+docker build -t cristian_suarez_dev/jupyterlab -f .docker/jupyter/Dockerfile . || exit 1
 
 docker run --rm --name jupyterlab --user root --env-file .env -p 8888:8888 -v $(pwd):/home/cristian_suarez_dev/jupyterlab -e NB_USER=cristian_suarez_dev -e NB_UID=$(id -u) -e NB_GID=$(id -g) cristian_suarez_dev/jupyterlab
